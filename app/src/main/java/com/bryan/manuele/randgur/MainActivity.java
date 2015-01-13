2d6d8884d23cfe099c68c98e48680389329f184c
Steelchopsticks <ManueleBryan@gmail.com>
2015-01-13 09:11:29 -0800

    Added a share button that shares the image. Untested.

--- app/src/main/java/com/bryan/manuele/randgur/MainActivity.java
+++ app/src/main/java/com/bryan/manuele/randgur/MainActivity.java
@@ -4,10 +4,13 @@ import android.app.ProgressDialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,9 +18,15 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class MainActivity extends Activity {
@@ -131,8 +140,47 @@ public class MainActivity extends Activity {
             case R.id.action_copy:
                 copyLinkToClipBoard();
                 return true;
+            case R.id.action_share:
+                shareImage();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void shareImage() {
+        Bitmap bitmap = images.get(slidePosition).bitmap;
+
+        try {
+            File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            File imageFile = new File(path, getCurrentTime()+ ".png");
+            FileOutputStream fileOutPutStream = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 80, fileOutPutStream);
+
+            fileOutPutStream.flush();
+            fileOutPutStream.close();
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.setType("image/png");
+            Uri uri = Uri.parse("file://" + imageFile.getAbsolutePath());
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Hello, This is test Sharing");
+            startActivity(Intent.createChooser(shareIntent, "Send your image"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getCurrentTime() {
+        Calendar calendar = new GregorianCalendar();
+
+        //monthdayhouryear
+        return (calendar.get(Calendar.MONTH) + 1) + "" +
+                calendar.get(Calendar.DAY_OF_MONTH) + "" +
+                calendar.get(Calendar.HOUR) + "" +
+                calendar.get(Calendar.MINUTE) + "" +
+                calendar.get(Calendar.SECOND);
     }
 
     private class LoadImage extends AsyncTask<String, String, Bitmap> {
